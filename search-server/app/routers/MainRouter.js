d6e0cb76e807d3c5c2134f5779c106d1ee01bf65
Benjamin Wagner <benjamin.wagner_ext@scout24.com>
2017-09-15 16:04:17 +0200

    added cors header

--- search-server/app/routers/MainRouter.js
+++ search-server/app/routers/MainRouter.js
@@ -1,23 +1,37 @@
 'use strict';
 
+var cors = require ('cors');
+
 module.exports = {
 	'/search-enable': {
 		get: {
+			before : [
+				cors()
+			],
 			action: 'SearchController@enableSearch'
 		}
 	},
 	'/search-disable': {
 		get: {
+			before : [
+				cors()
+			],
 			action: 'SearchController@disableSearch'
 		}
 	},
 	'/last-search': {
 		get: {
+			before : [
+				cors()
+			],
 			action: 'SearchController@getLastSearch'
 		}
 	},
 	'/search': {
 		get: {
+			before : [
+				cors()
+			],
 			action: 'SearchController@search'
 		}
 	}
