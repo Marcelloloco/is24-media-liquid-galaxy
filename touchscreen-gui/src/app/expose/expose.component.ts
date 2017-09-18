4da3692a938f58e62391c77f1de6976779919d26
Jakob Anders <jakob.anders@immobilienscout24.de>
2017-09-18 13:53:13 +0200

    [ja] add first pass of frontend mappings

--- touchscreen-gui/src/app/expose/expose.component.ts
+++ touchscreen-gui/src/app/expose/expose.component.ts
@@ -10,6 +10,7 @@ import {ActivatedRoute} from "@angular/router";
 
 export class ExposeComponent implements OnInit {
     expose: Object;
+    detailsGroups: Array<Array<Array<Object>>>;
 
     constructor(
         private exposeService: ExposeService,
@@ -17,11 +18,61 @@ export class ExposeComponent implements OnInit {
     ) {
     }
 
+    private detailFor(value, caption) {
+        return { key: caption, value: value };
+    }
+
     ngOnInit() {
         let id = this.route.snapshot.paramMap.get('id');
         this.exposeService.get(id)
             .subscribe(expose => {
-                this.expose = expose['expose.expose']
+                this.expose = expose['expose.expose'];
+                let realEstate = this.expose['realEstate'];
+                this.detailsGroups = [
+                    [
+                        [
+                            this.detailFor(realEstate.apartmentType, "Typ"),
+                            this.detailFor(realEstate.floor, "Etage"),
+                            this.detailFor(realEstate.livingSpace, "Wohnfläche"),
+                            this.detailFor("???", "Bezugsfrei"),
+                            this.detailFor(realEstate.numberOfRooms, "Zimmer"),
+                            this.detailFor(realEstate.numberOfBedRooms, "Schlafzimmer"),
+                        ],
+                        [
+                            this.detailFor(realEstate.numberOfBathRooms, "Badezimmer"),
+                            this.detailFor(realEstate.cellar, "Keller"),
+                            this.detailFor(realEstate.balcony, "Balkon / Terasse"),
+                            this.detailFor(realEstate.guestToilet, "Gäste-WC"),
+                            this.detailFor("???", "Internet"),
+                        ],
+                    ],
+                    [
+                        [
+                            this.detailFor("???", "Kaltmiete"),
+                            this.detailFor("???", "Nebenkosten")
+                        ],
+                        [
+                            this.detailFor("???", "Heizkosten"),
+                            this.detailFor("???", "Gesamtmiete"),
+                            this.detailFor("???", "Kaution o. Genossenschaftsanteile"),
+                        ]
+                    ],
+                    [
+                        [
+                            this.detailFor("???", "Baujahr"),
+                            this.detailFor("???", "Objektzustand"),
+                            this.detailFor("???", "Denkmalschutz"),
+                            this.detailFor("???", "Ausstattung"),
+                            this.detailFor("???", "Zentralheizung"),
+                        ],
+                        [
+                            this.detailFor("???", "Energieträger"),
+                            this.detailFor("???", "Energieausweiß"),
+                            this.detailFor("???", "Energieverbrauchswert"),
+                        ]
+
+                    ]
+                ];
             });
     }
 
