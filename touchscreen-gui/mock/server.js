68d200efba07aecbc15bc69fea45aba0254a813c
jkarlsson <johan.karlsson@scout24.com>
2017-09-18 17:28:34 +0200

    add cors support to mock server

--- touchscreen-gui/mock/server.js
+++ touchscreen-gui/mock/server.js
@@ -2,7 +2,13 @@ var http = require('http');
 var fs = require('fs');
 var respBody = fs.readFileSync('resp.json');
 
+
 http.createServer(function (req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Request-Method', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+
   res.writeHead(200, {'Content-Type': 'application/json'});
   res.end(respBody);
 }).listen(9615);
