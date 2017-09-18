2ee2601f530d64d86dc81ae3d263a980ae19b109
Jakob Anders <jakob.anders@immobilienscout24.de>
2017-09-18 11:17:04 +0200

    [ja/bl] ... and actually add all the files

---
+++ touchscreen-gui/src/app/expose/expose.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-expose',
+  templateUrl: './expose.component.html',
+  styleUrls: ['./expose.component.css']
+})
+export class ExposeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ touchscreen-gui/src/app/search/search.component.ts
@@ -0,0 +1,71 @@
+import { Component } from '@angular/core';
+import {MdSliderChange} from '@angular/material';
+import {City} from '../city';
+import {CitiesNavigationService} from '../cities-navigation.service';
+import {Observable} from 'rxjs/Rx';
+import {PropertiesListService} from '../properties-list.service';
+import {Property} from '../property';
+import {SearchService} from '../search.service';
+
+@Component({
+    selector: 'app-search',
+    templateUrl: './search.component.html',
+    styleUrls: ['./search.component.less']
+})
+export class SearchComponent {
+    cities = City;
+    city = City.Berlin;
+    isRent = true;
+    price = 800;
+    space = 70;
+
+    properties: Property[] = [];
+
+    constructor(private citiesNavigationService: CitiesNavigationService,
+                private propertiesListService: PropertiesListService,
+                private searchService: SearchService) {
+        this.startPollingProperties(propertiesListService);
+    }
+
+    private startPollingProperties(propertiesListService: PropertiesListService) {
+        Observable.interval(100)
+            .switchMap(() => propertiesListService.getCurrentProperties())
+            .subscribe((data) => {
+                if (JSON.stringify(data) !== JSON.stringify(this.properties)) {
+                    this.properties = data;
+                }
+            });
+    }
+
+    public cityChanged(city: City) {
+        this.city = city;
+        this.citiesNavigationService.navigate(this.city);
+    }
+
+    public spaceChanged(event: MdSliderChange) {
+        this.space = event.value;
+    }
+
+    public priceChanged(event: MdSliderChange) {
+        this.price = event.value;
+    }
+
+    public typeChanged(isRent: boolean) {
+        this.isRent = isRent;
+        if (isRent) {
+            this.price = 2000;
+        } else {
+            this.price = 100000;
+        }
+    }
+
+    public search() {
+        this.searchService.search(this.isRent, this.price, this.space);
+    }
+
+    public opened(a:any) {
+        console.log(a);
+    }
+
+
+}
